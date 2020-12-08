21dea4b4250a97f95dd10a74dcb5d7e9e1ba3804
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0000

    charlotte: Migrate to Health 2.1 HAL
    Change-Id: Ic6302844ac58c16713b43b6871a705b9b4749a96

--- device.mk
+++ device.mk
@@ -91,6 +91,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libion
 
+# Health
+PRODUCT_PACKAGES += \  
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Hisi init
 PRODUCT_PACKAGES += \
     hisi_init
