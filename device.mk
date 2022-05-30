dad7cf2eadc967ef37a46e451b033250d2886785
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    charlotte: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- device.mk
+++ device.mk
@@ -61,11 +61,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm-service.clearkey
-
-PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.2.vendor
 
 PRODUCT_PACKAGES += \
