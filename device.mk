c581f0372a48c005fd618c5d2e94dc3aecc2a1e1
LuK1337 <priv.luk@gmail.com>
2024-01-02 16:22:15 +0100

    charlotte: overlay-lineage: Enable config_fingerprintPostResetRunnableForAllClients
    Change-Id: I0ffd3fac173c8abbcba5fb3dac54f6a17e701552
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -20,7 +20,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Ramdisk
 PRODUCT_PACKAGES += \
