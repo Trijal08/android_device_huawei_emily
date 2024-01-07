b7a382a3cc9bb55a953810ab62e844de59a6443b
arco <arco68@gmail.com>
2024-01-07 21:24:44 +0100

    charlotte: Silence clang error during kernel compile
    * clang-14: error: unable to execute command: Executable "ld" doesn't exist!
    
    Change-Id: I1ebf0fb665704424a8ad3a375c546c6cca44dc0b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset 
 
 TARGET_KERNEL_CLANG_VERSION := r416183b
 TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
+TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_LLVM_BINUTILS := false
 
 TARGET_KERNEL_SOURCE := kernel/huawei/kirin970
