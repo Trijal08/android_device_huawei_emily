ad40142889f50e09617339419d19306edd713398
Michael Bestas <mkbestas@lineageos.org>
2024-01-07 21:24:08 +0100

    charlotte: Set TARGET_KERNEL_CLANG_VERSION to r416183b
    Change-Id: Ic1eb6f7e3cb54bdb67da2fb963426a240f2edda3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,8 @@ BOARD_KERNEL_CMDLINE := loglevel=4 page_tracker=on unmovable_isolate1=2:192M,3:2
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --second_offset $(BOARD_SECOND_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --header_version $(BOARD_BOOT_HEADER_VERSION)
 
+TARGET_KERNEL_CLANG_VERSION := r416183b
+TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_LLVM_BINUTILS := false
 
 TARGET_KERNEL_SOURCE := kernel/huawei/kirin970
