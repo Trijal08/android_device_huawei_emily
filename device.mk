463e7a1e4e38eedcabdde46affa159d74d12d575
Daniel Rosenberg <drosen@google.com>
2024-01-31 10:26:32 +0100

    charlotte: Enable Project ID Quota support on userdata
    * These features enable wide inodes on userdata, which is
      required for project ID quotas. Wide inodes can only be
      set at MKFS time.
    
    * This also enables casefolding, which is required for the
      removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Setup dalvik-heap
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # AAPT conf
 PRODUCT_AAPT_CONFIG := normal 
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
