7c8a01fe919eee0ba0b51f96250f5e6ef27ccec9
Giovanni Ricca <giovanniricca@proton.me>
2024-03-11 13:44:22 +0100

    charlotte: Create `modem_fw` mountpoint
    Change-Id: Ic084904caf4e4ccd5cbc3481977478992449ab9b

--- Android.mk
+++ Android.mk
@@ -10,4 +10,13 @@ ifneq ($(filter charlotte,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+include $(CLEAR_VARS)
+
+FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/modem/modem_fw
+$(FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/modem/modem_fw
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT)
+
 endif
