2435205be3fecc45a4a6d299676be50143fba251
R0rt1z2 <me@r0rt1z2.com>
2024-03-20 13:22:55 +0100

    charlotte: Build hisi livedisplay sysfs service

--- device.mk
+++ device.mk
@@ -116,6 +116,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Livedisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.hisi
+    
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
