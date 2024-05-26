145eb30f6d1d4a9e687c73a021a5b3c61b32107e
Huawei_Dev <surdu_petru@yahoo.com>
2024-05-26 15:25:41 +0200

    charlotte: build keymaster hidl

--- device.mk
+++ device.mk
@@ -42,6 +42,11 @@ PRODUCT_PACKAGES += \
 # Hisi init
 PRODUCT_PACKAGES += \
     hisi_init
+    
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Memtrack
 PRODUCT_PACKAGES += \
