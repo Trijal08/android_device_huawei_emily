13b198cd0aed79190c0e63fadcc525335240a18d
Huawei_Dev <surdu_petru@yahoo.com>
2025-03-12 22:36:08 +0100

    charlotte: clean init scripts

--- device.mk
+++ device.mk
@@ -18,13 +18,15 @@ PRODUCT_PACKAGES += \
     fstab.kirin970 \
     fstab.kirin970.ramdisk \
     fstab.modem \
+    init.connectivity.kirin970.rc \
+    init.hisi.kirin970.rc \
+    init.kirin970.rc \
+    init.modem.kirin970.rc \
+    init.usb.kirin970.rc \
     ueventd.kirin970.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.kirin970.rc:$(TARGET_RECOVERY_OUT)/root/init.recovery.kirin970.rc
-
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/etc/,$(TARGET_COPY_OUT_VENDOR)/etc/init/hw)
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/huawei/charlotte/charlotte-vendor.mk)
