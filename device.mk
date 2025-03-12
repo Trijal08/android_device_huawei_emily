15fb4b0dbc0d7f5d9a4d51c3362318594980a52c
Huawei_Dev <surdu_petru@yahoo.com>
2025-03-12 18:29:32 +0100

    charlotte: import stock EMUI12 init scripts

--- device.mk
+++ device.mk
@@ -13,5 +13,18 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.kirin970 \
+    fstab.kirin970.ramdisk \
+    fstab.modem \
+    ueventd.kirin970.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.recovery.kirin970.rc:$(TARGET_RECOVERY_OUT)/root/init.recovery.kirin970.rc
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/etc/,$(TARGET_COPY_OUT_VENDOR)/etc/init/hw)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/huawei/charlotte/charlotte-vendor.mk)
