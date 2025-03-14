e164ed9272c9f42a0398c851a44c48a3c31369a9
Huawei_Dev <surdu_petru@yahoo.com>
2025-03-14 19:22:06 +0100

    charlotte: build usb service

--- device.mk
+++ device.mk
@@ -197,6 +197,10 @@ PRODUCT_SHIPPING_API_LEVEL := 27
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
