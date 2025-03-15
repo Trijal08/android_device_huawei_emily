91a3e74690edcdddaa97984068d8c8eb44e6f1de
Huawei_Dev <surdu_petru@yahoo.com>
2025-03-15 09:21:55 +0100

    charlote: build radio dependency

--- device.mk
+++ device.mk
@@ -210,6 +210,15 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor
+
+PRODUCT_PACKAGES += \
+    librilutils
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.kirin970 \
