1c67af040ba84904176cd67382783b8a3a5186fb
Huawei_Dev <surdu_petru@yahoo.com>
2025-03-28 19:25:26 +0100

    charlotte: drop thermal service

--- device.mk
+++ device.mk
@@ -262,11 +262,6 @@ PRODUCT_SOONG_NAMESPACES += \
 #    vendor.huawei.hardware.libteec@2.0 \
 #    vendor.huawei.hardware.libteec@2.0.vendor
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.hisi
