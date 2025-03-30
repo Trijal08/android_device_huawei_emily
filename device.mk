267f8713a07640e873421f499fa038a3c506d8bc
Roger Ortiz <me@r0rt1z2.com>
2025-03-30 13:47:45 +0200

    charlotte: Switch to stock keymaster impl * Fixes keystore.
    Change-Id: I55dfa1049d3e71e675175da6806c40950393e7fc

--- device.mk
+++ device.mk
@@ -127,8 +127,12 @@ PRODUCT_PACKAGES += \
     
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
+
+PRODUCT_PACKAGES += \
+    libkeymaster3device.vendor \
+    libkeystore-engine-wifi-hidl \
+    libkeystore-wifi-hidl
 
 # Light
 PRODUCT_PACKAGES += \
