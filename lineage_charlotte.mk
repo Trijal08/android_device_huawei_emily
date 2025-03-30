10772ccfcf44ff22499ffc34da48abf2c66277df
Huawei_Dev <surdu_petru@yahoo.com>
2025-03-30 17:07:00 +0200

    charlotte: import stock APN

--- lineage_charlotte.mk
+++ lineage_charlotte.mk
@@ -5,6 +5,7 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(LOCAL_PATH)/pre-base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
---
+++ pre-base.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
