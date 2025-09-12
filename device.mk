079a3f67ece9c0d643cdbc8f74e8c85d40558859
Raphael Mounier <mounierr07@gmail.com>
2025-09-12 20:19:39 +0200

    kirin970: fix failed to activate Wifi Hotspot

--- device.mk
+++ device.mk
@@ -360,6 +360,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
+    libwpa_client \
+    wificond \
     wpa_supplicant \
     wpa_supplicant.conf 
 
