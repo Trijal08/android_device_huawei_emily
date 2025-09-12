4be45dc7e2678f9b1fb8d47dbc357c6bce027c04
Raphael Mounier <mounierr07@gmail.com>
2025-09-12 16:24:52 +0200

    charlotte: add wifi drivers firmware path
    charlotte have bcm4359

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,5 +168,10 @@ BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 CONFIG_DRIVER_NL80211		:= y
 
+# Wifi firmware for charlotte hw/eu
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcm4359_apsta_hw.bin"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/fw_bcm4359_hw.bin"
+
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
