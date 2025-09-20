9f0ca7058a4979cc8f24889e307384c5431fdc50
Raphael Mounier <mounierr07@gmail.com>
2025-09-20 14:53:43 +0200

    kirin970: Convert Vulkan symlinks to install_symlink targets
    Co-Authored-By: Surdu Petru <surdu_petru@yahoo.com>

--- Android.mk
+++ Android.mk
@@ -19,29 +19,4 @@ $(FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT)
 
-EGL_LIBS := hw/vulkan.kirin970.so
-
-EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
-$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL 32 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so $@
-
-EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(EGL_LIBS))
-$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL 64 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
-
-NATIVE_PACKAGES_FIXUP := $(TARGET_OUT_VENDOR)/etc/native_packages.xml
-$(NATIVE_PACKAGES_FIXUP): $(TARGET_OUT_VENDOR)/etc/native_packages.bin
-	@echo "Move vendor native_packages.bin to native_packages.xml"
-	$(hide) mv $(TARGET_OUT_VENDOR)/etc/native_packages.bin $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(NATIVE_PACKAGES_FIXUP)
-
 endif
