f765259fc42270065fa09cb8098622e9bdda9caa
Trijal Saha <trijalsaha2012@gmail.com>
2026-01-14 01:09:10 -0500

    Import device_framework_matrix.xml from lineage-22.2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,7 +159,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/prebuilts/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/prebuilts/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/prebuilts/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(DEVICE_PATH)/device_framework_matrix.xml
 
 # Vulkan
 TARGET_USES_VULKAN := true
